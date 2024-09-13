db948e9250478dcce8063fbb67eb16a50e236fb3
Ducnna <loadoftherings213@gmail.com>
2024-09-13 15:40:22 +0700

    This is my Practical work 7 and 8

--- WeatherActivity/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ WeatherActivity/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -3,6 +3,10 @@ package vn.edu.usth.weather;
 import android.os.Bundle;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class WeatherActivity extends AppCompatActivity {
     private static final String TAG = "WeatherActivity";
@@ -12,13 +16,27 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
-        // Add this block of code to dynamically add the ForecastFragment
-        if (savedInstanceState == null) { // Check to prevent adding the fragment multiple times
-            ForecastFragment forecastFragment = new ForecastFragment();
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.forecastFragment, forecastFragment)
-                    .commit();
-        }
+        // Set up the ViewPager2 and adapter for multiple WeatherAndForecastFragments
+        ViewPager2 viewPager = findViewById(R.id.viewPager);
+        WeatherPagerAdapter adapter = new WeatherPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> {
+                    // Set the title for each tab
+                    switch (position) {
+                        case 0:
+                            tab.setText("HANOI, VIETNAM");
+                            break;
+                        case 1:
+                            tab.setText("PARIS, FRANCE");
+                            break;
+                        case 2:
+                            tab.setText("TOULOUSE, FRANCE");
+                            break;
+                    }
+                }).attach();
     }
 
     @Override
